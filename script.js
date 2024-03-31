d4e1a1d85558518423e81d13517bcbf54d68f268
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-04-01 00:40:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function makeid(l) {
   // write your code here
+	let result = ' '; 
+        for(let i = 0; i < l; i++) { 
+            result +=  
+            String.fromCharCode(97 + Math.floor(Math.random() * 26)); 
+        } 
+	return result;
 }
-
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
